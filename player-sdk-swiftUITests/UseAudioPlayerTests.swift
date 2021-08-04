0551e7fb7f89d32491133b79dc7f8453c00b4f61
Florian Nowotny <Florian.Nowotny@nacamar.de>
2021-08-04 11:10:01 +0200

    0.12.2 (38)

--- player-sdk-swiftUITests/UseAudioPlayerTests.swift
+++ player-sdk-swiftUITests/UseAudioPlayerTests.swift
@@ -202,8 +202,7 @@ class UseAudioPlayerTests: XCTestCase {
      The audio codec opus is supported
      */
     func test06_PlayOpus() {
-//        let opusEndpoint = MediaEndpoint(mediaUri: "https://dradio-dlf-live.cast.addradio.de/dradio/dlf/live/opus/high/stream.opus")
-        let opusEndpoint = MediaEndpoint(mediaUri: "file:///Users/florian/Music/audioTests/OGG-OPUS/sample_opus.opus")
+        let opusEndpoint = MediaEndpoint(mediaUri: "https://dradio-dlf-live.cast.addradio.de/dradio/dlf/live/opus/high/stream.opus")
         do {
             try AudioPlayer.open(for: opusEndpoint, listener: playerListener) {
                 (control) in
